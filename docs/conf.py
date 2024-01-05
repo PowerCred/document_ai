7d87d0112ee0f84ecdda2d99d6b472c76f72b1c3
shrinivdeshmukh <shrinivas.deshmukh11@gmail.com>
2024-01-05 08:41:32 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -128,7 +128,7 @@ latex_elements = {
 # [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'document_ai.tex',
-     'PowerCred Document AI Documentation',
+     'PowerCreds Document AI Documentation',
      'Shrinivas Deshmukh', 'manual'),
 ]
 
